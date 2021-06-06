d1880fa97761c91ad3350adb636180af8f9b7e2c
Satham <Satham@SATHAM>
2021-06-06 19:46:01 +0530

    fifth program

---
+++ test/src/hit/d4/OddEvenCheck.java
@@ -0,0 +1,13 @@
+package hit.d4;
+
+public class OddEvenCheck {
+
+	public static void main(String[] args) {
+		int num=157;
+		if ( num % 2 == 0 )
+		System.out.println("The number is "+ num +" and it's even number");
+		else
+		System.out.println("The number is "+ num +" and it's odd number");
+	}
+
+}
