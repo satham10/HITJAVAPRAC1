caeb50e4aa92e94aab153b0dd0f849445db7ad81
Satham <Satham@SATHAM>
2021-06-06 18:25:09 +0530

    First Program

---
+++ test/src/hit/d4/TwoNumSum.java
@@ -0,0 +1,11 @@
+package hit.d4;
+
+public class TwoNumSum {
+
+	public static void main(String[] args) {
+		int num1 = 100, num2 = 200, sum;
+		sum = num1 + num2;
+		System.out.println("Sum of two numbers: "+sum);
+	}
+
+}
