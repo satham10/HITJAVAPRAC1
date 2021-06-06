fc832268f6e3a4cabab2c9e7c365389cb89ac7a4
Satham <Satham@SATHAM>
2021-06-06 18:33:10 +0530

    Second Program

---
+++ test/src/hit/d4/QuotientRemainder.java
@@ -0,0 +1,14 @@
+package hit.d4;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		int num1 = 70, num2 = 6;
+		int quotient = num1 / num2;
+		int remainder = num1 % num2;
+		System.out.println("Quotient = " + quotient);
+		System.out.println("Remainder = " + remainder);
+
+	}
+
+}
